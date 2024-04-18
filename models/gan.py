438422b649322d96a91d7787c510e13f72c35b8a
Lena Huber <lena.huber@commend.com>
2024-04-18 21:35:04 +0200

    new gan

--- models/gan.py
+++ models/gan.py
@@ -1,169 +1,83 @@
 import numpy as np
+import tensorflow as tf
+from keras import layers, models
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import preprocessing
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import precision_score, recall_score, f1_score,\
-                            accuracy_score, balanced_accuracy_score,classification_report,\
-                            plot_confusion_matrix, confusion_matrix
-from sklearn.model_selection import KFold, GridSearchCV
-from sklearn.model_selection import train_test_split
-from sklearn.utils import shuffle
-
-import lightgbm as lgb
-from tensorflow.keras.layers import Input, Dense, Reshape, Flatten, Dropout, multiply, Concatenate
-from tensorflow.keras.layers import BatchNormalization, Activation, Embedding, ZeroPadding2D, LeakyReLU
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.optimizers import Adam
-from tensorflow.keras.initializers import RandomNormal
-import tensorflow.keras.backend as K
-
-
 np.random.seed(42)
+
+import os
+
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
 
 
 class GAN():
+
+    def __init__(self, data, noise, epochs) -> None:
+       self.data = data
+       self.noise = noise
+       self.epochs = epochs
+
+    def create_generator(self):
+        model = models.Sequential(name="generator_model")
+        model.add(layers.Dense(15, activation='relu',input_dim=self.data.shape[1]))
+        model.add(layers.Dense(30, activation='relu'))
+        model.add(layers.Dense(self.data.shape[1], activation='tanh'))
+        return model
     
-    """
-    Class containing 3 methods (and __init__): generator, discriminator and train.
-    Generator is trained using random noise and label as inputs. Discriminator is trained
-    using real/fake samples and labels as inputs.
-    """
     
-    def __init__(self,latent_dim=32, out_shape=14):
-        
-        self.latent_dim = latent_dim
-        self.out_shape = out_shape 
-        self.num_classes = 2
-        # using Adam as our optimizer
-        optimizer = Adam(0.0002, 0.5)
-        
-        # building the discriminator
-        self.discriminator = self.discriminator()
-        self.discriminator.compile(loss=['binary_crossentropy'],
-                                   optimizer=optimizer,
-                                   metrics=['accuracy'])
-
-        # building the generator
-        self.generator = self.generator()
-
-        noise = Input(shape=(self.latent_dim,))
-        label = Input(shape=(1,))
-        gen_samples = self.generator([noise, label])
-        
-        # we don't train discriminator when training generator
-        self.discriminator.trainable = False
-        valid = self.discriminator([gen_samples, label])
-
-        # combining both models
-        self.combined = Model([noise, label], valid)
-        self.combined.compile(loss=['binary_crossentropy'],
-                              optimizer=optimizer,
-                             metrics=['accuracy'])
-
-
-    def generator(self):
-        init = RandomNormal(mean=0.0, stddev=0.02)
-        model = Sequential()
-
-        model.add(Dense(128, input_dim=self.latent_dim))
-        model.add(Dropout(0.2))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(Dense(256))
-        model.add(Dropout(0.2))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(Dense(512))
-        model.add(Dropout(0.2))
-        model.add(LeakyReLU(alpha=0.2))
-        model.add(BatchNormalization(momentum=0.8))
-
-        model.add(Dense(self.out_shape, activation='tanh'))
-
-        noise = Input(shape=(self.latent_dim,))
-        label = Input(shape=(1,), dtype='int32')
-        label_embedding = Flatten()(Embedding(self.num_classes, self.latent_dim)(label))
-        
-        model_input = multiply([noise, label_embedding])
-        gen_sample = model(model_input)
-
-        return Model([noise, label], gen_sample, name="Generator")
-
+    def create_discriminator(self):
+        model = models.Sequential(name="discriminator_model")
+        model.add(layers.Dense(25, activation='relu',input_dim=self.data.shape[1]))
+        model.add(layers.Dense(50, activation='relu'))
+        model.add(layers.Dense(1, activation='sigmoid'))
+        model.compile(loss='binary_crossentropy',optimizer='adam', metrics=['accuracy'])
+        return model
     
-    def discriminator_model():
-        model = Sequential([
-            layers.Dense(64, activation='relu', input_shape=(7,)),  # 7 stats for each Pokémon
-            layers.Dense(64, activation='relu'),
-            layers.Dense(1, activation='sigmoid')  # Output 1 for real and 0 for synthetic
-        ])
+    def compile(self, generator, discriminator):
+        discriminator.trainable = False
+        generator.trainable = True
+        model = models.Sequential(name="GAN")
+        model.add(generator)
+        model.add(discriminator)
+        model.compile(loss='binary_crossentropy', optimizer='adam')
         return model
 
-
-    def train(self, X_train, y_train, pos_index, neg_index, epochs, sampling=False, batch_size=32, sample_interval=100, plot=True): 
-        
-        # though not recommended, defining losses as global helps as in analysing our cgan out of the class
-        global G_losses
-        global D_losses
-        
-        G_losses = []
-        D_losses = []
-        # Adversarial ground truths
-        valid = np.ones((batch_size, 1))
-        fake = np.zeros((batch_size, 1))
-
-        for epoch in range(epochs):
+    def train(self, generator, discriminator, gan):
+        for epoch in range(self.epochs):
             
-            # if sampling==True --> train discriminator with 8 sample from postivite class and rest with negative class
-            if sampling:
-                idx1 = np.random.choice(pos_index, 8)
-                idx0 = np.random.choice(neg_index, batch_size-8)
-                idx = np.concatenate((idx1, idx0))
-            # if sampling!=True --> train discriminator using random instances in batches of 32
-            else:
-                idx = np.random.choice(len(y_train), batch_size)
-            samples, labels = X_train[idx], y_train[idx]
-            samples, labels = shuffle(samples, labels)
-            
-            # Sample noise as generator input
-            noise = np.random.normal(0, 1, (batch_size, self.latent_dim))
-            gen_samples = self.generator.predict([noise, labels])
-
-            # label smoothing
-            if epoch < epochs//1.5:
-                valid_smooth = (valid+0.1)-(np.random.random(valid.shape)*0.1)
-                fake_smooth = (fake-0.1)+(np.random.random(fake.shape)*0.1)
-            else:
-                valid_smooth = valid 
-                fake_smooth = fake
-                
             # Train the discriminator
-            self.discriminator.trainable = True
-            d_loss_real = self.discriminator.train_on_batch([samples, labels], valid_smooth)
-            d_loss_fake = self.discriminator.train_on_batch([gen_samples, labels], fake_smooth)
-            d_loss = 0.5 * np.add(d_loss_real, d_loss_fake)
+            generated_data = generator.predict(self.noise)
+            generated_data = generated_data.astype(np.float32)
 
-            # Train Generator
-            self.discriminator.trainable = False
-            sampled_labels = np.random.randint(0, 2, batch_size).reshape(-1, 1)
+            # Prepare real and synthetic labels
+            real_labels = np.ones((self.data.shape[0], 1), dtype=np.float32)
+            fake_labels = np.zeros((self.data.shape[0], 1), dtype=np.float32)
+            labels = np.concatenate([real_labels, fake_labels])
+            X = np.concatenate([self.data, generated_data])
+            discriminator.trainable = True
+            print(X)
+            print(labels)
+            d_loss , _ = discriminator.train_on_batch(X, labels)
+
             # Train the generator
-            g_loss = self.combined.train_on_batch([noise, sampled_labels], valid)
+            g_loss = gan.train_on_batch(self.noise, np.ones(self.data.shape[0]))
 
-            if (epoch+1)%sample_interval==0:
-                print('[%d/%d]\tLoss_D: %.4f\tLoss_G: %.4f'
-                  % (epoch, epochs, d_loss[0], g_loss[0]))
-            G_losses.append(g_loss[0])
-            D_losses.append(d_loss[0])
-            if plot:
-                if epoch+1==epochs:
-                    plt.figure(figsize=(10,5))
-                    plt.title("Generator and Discriminator Loss")
-                    plt.plot(G_losses,label="G")
-                    plt.plot(D_losses,label="D")
-                    plt.xlabel("iterations")
-                    plt.ylabel("Loss")
-                    plt.legend()
-                    plt.show()
+
+            print('>%d, d1=%.3f, d2=%.3f' %(epoch+1, d_loss, g_loss))
+
+        return generator
+    
+
+def main():
+    
+    df_pokemon= pd.read_csv("./data/Pokemon.csv")
+    df_pokemon = df_pokemon.drop(columns=['Name', 'Total'], axis=1)
+    df_pokemon.head()
+    noise = np.random.normal(0, 1, df_pokemon.shape) 
+    gan = GAN(data=df_pokemon, noise=noise, epochs=2)
+    generator = gan.create_generator()
+    discriminator = gan.create_discriminator()
+    gan_model = gan.compile(generator=generator, discriminator=discriminator)
+    trained_gan = gan.train(generator=generator,discriminator=discriminator, gan=gan)
+
+    
+main()
