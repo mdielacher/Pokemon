78d301b55bcb7e6d2871a4f0594b99b8c1b4a702
Lena Huber <lena.huber@commend.com>
2024-04-19 14:20:26 +0200

    GAN done next eval

--- models/gan.py
+++ models/gan.py
@@ -5,15 +5,16 @@ from keras import layers, models
 class GAN():
     """Class to train a GAN model"""
 
-    def __init__(self, data, noise, epochs) -> None:
+    def __init__(self, data, noise_dim, epochs, batch_size) -> None:
         self.data = data
-        self.noise = noise
+        self.noise_dim = noise_dim
         self.epochs = epochs
+        self.batch_size = batch_size
 
     def create_generator(self) -> models.Sequential:
         """Generate generator"""
         model = models.Sequential(name="generator_model")
-        model.add(layers.Dense(15, activation='relu',input_dim=self.data.shape[1]))
+        model.add(layers.Dense(15, activation='relu',input_dim=self.noise_dim))
         model.add(layers.Dense(30, activation='relu'))
         model.add(layers.Dense(self.data.shape[1], activation='sigmoid'))
         return model
@@ -48,23 +49,28 @@ class GAN():
 
     def train(self, generator, discriminator, gan):
         for epoch in range(self.epochs):
+            noise = np.random.normal(0, 1, (self.batch_size, self.noise_dim))
             
             # Train the discriminator
-            generated_data = generator.predict(self.noise)
+            generated_data = generator.predict(noise)
             generated_data = generated_data.astype(np.float32)
 
+            idx = np.random.randint(0, self.data.shape[0], self.batch_size)
+            real_data = self.data.iloc[idx]
+
             # Prepare real and synthetic labels
-            real_labels = np.ones((self.data.shape[0], 1), dtype=np.float32)
-            fake_labels = np.zeros((self.data.shape[0], 1), dtype=np.float32)
+            real_labels = np.ones((self.batch_size, 1), dtype=np.float32)
+            fake_labels = np.zeros((self.batch_size, 1), dtype=np.float32)
             labels = np.concatenate([real_labels, fake_labels])
-            X = np.concatenate([self.data, generated_data])
+            X = np.concatenate([real_data, generated_data])
             discriminator.trainable = True
             print(len(X))
             print(len(labels))
             d_loss , _ = discriminator.train_on_batch(X, labels)
 
             # Train the generator
-            g_loss = gan.train_on_batch(self.noise, np.ones(self.data.shape[0]))
+            noise = np.random.normal(0, 1, (self.batch_size, self.noise_dim))
+            g_loss = gan.train_on_batch(noise, np.ones(self.batch_size))
 
 
             print('>%d, d1=%.3f, d2=%.3f' %(epoch+1, d_loss, g_loss))
@@ -73,27 +79,27 @@ class GAN():
     
     #TODO: generator evaluerien
     
-    def evaluate_discriminator(self, generator, discriminator):
-        # Generate fake data
-        generated_data = generator.predict(self.noise)
-        generated_data = generated_data.astype(np.float32)
+    # def evaluate_discriminator(self, generator, discriminator):
+    #     # Generate fake data
+    #     generated_data = generator.predict(self.noise)
+    #     generated_data = generated_data.astype(np.float32)
 
-        # Real data
-        real_data = self.data
+    #     # Real data
+    #     real_data = self.data
 
-        # Labels for real and fake data
-        real_labels = np.ones((real_data.shape[0], 1), dtype=np.float32)
-        fake_labels = np.zeros((generated_data.shape[0], 1), dtype=np.float32)
+    #     # Labels for real and fake data
+    #     real_labels = np.ones((real_data.shape[0], 1), dtype=np.float32)
+    #     fake_labels = np.zeros((generated_data.shape[0], 1), dtype=np.float32)
 
-        # Evaluate discriminator on real data
-        real_loss, real_accuracy = discriminator.evaluate(real_data, real_labels, verbose=0)
+    #     # Evaluate discriminator on real data
+    #     real_loss, real_accuracy = discriminator.evaluate(real_data, real_labels, verbose=0)
 
-        # Evaluate discriminator on fake data
-        fake_loss, fake_accuracy = discriminator.evaluate(generated_data, fake_labels, verbose=0)
+    #     # Evaluate discriminator on fake data
+    #     fake_loss, fake_accuracy = discriminator.evaluate(generated_data, fake_labels, verbose=0)
 
-        print(f'Real Data Accuracy: {real_accuracy*100:.2f}%')
-        print(f'Fake Data Accuracy: {fake_accuracy*100:.2f}%')
-        print(f'Overall Accuracy: {(real_accuracy + fake_accuracy) * 50:.2f}%')  # Average of real and fake accuracy
+    #     print(f'Real Data Accuracy: {real_accuracy*100:.2f}%')
+    #     print(f'Fake Data Accuracy: {fake_accuracy*100:.2f}%')
+    #     print(f'Overall Accuracy: {(real_accuracy + fake_accuracy) * 50:.2f}%')  # Average of real and fake accuracy
 
     
         
